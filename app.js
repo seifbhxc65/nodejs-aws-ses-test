7ed737b3c64c76f3e6f2bde9951130aed8c944ad
Seif Belhaj <seifedine.belhaj@gmail.com>
2024-02-11 15:20:47 +0100

    added backend part

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mailer = require('./routes/router');
+app.use(cors());
+app.use(express.json()) ;
+app.use('',express.static('public'));
+app.use( mailer);
+    
+const port = 3000;  
+app.get('/', (req, res) => {
+     res.send('Hello World!');
+    });    
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`);
+    });
---
+++ routes/router.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const aws = require('aws-sdk');
+const e = require('express');
+//SET UP DOTENV
+require('dotenv').config();
+console.log(process.env.AWS_ACCESS_KEY_ID);
+console.log(process.env.AWS_SECRET_ACCESS_KEY);
+
+const ses = new aws.SES({
+    region: 'us-east-1',
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+router.post("/send-email", function (req, res) {
+    const { email, message, name } = req.body;
+    console.log("req", req.body);
+    sesTest("saif40340@gmail.com", email, message, name)
+      .then((val) => {
+        console.log("got this back", val);
+        res.send("Successfully Sent Email");
+      })
+      .catch((err) => {
+        res.send(err);
+  
+        console.log("There was an error!", err);
+      });
+  });
+  
+  function sesTest(emailTo, emailFrom, message, name) {
+    console.log(`Sending email to: ${emailTo}`);
+    var params = {
+      Destination: {
+        ToAddresses: [emailTo]
+      },
+      Message: {
+        Body: {
+          Text: { Data: "From Contact Form: " + name + "\n " + message }
+        },
+  
+        Subject: { Data: "From: " + emailFrom }
+      },
+      Source: "seifedine.belhaj@gmail.com"
+    };
+  
+    return ses.sendEmail(params).promise();
+  }
+module.exports = router;
